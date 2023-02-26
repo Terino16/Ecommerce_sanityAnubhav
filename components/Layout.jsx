6dbf0374b8664f3f567711ea8a3d12e115e9c19f
Terino16 <123401184+Terino16@users.noreply.github.com>
2023-02-26 22:06:22 +0530

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Boat Dubne wali hai</title>
+        <title>Boat </title>
       </Head>
       <header>
         <Navbar />
